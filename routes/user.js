55f174cf670094f8c88171076f080e418a1b3840
cyryne <125557012+cyryne@users.noreply.github.com>
2023-11-24 01:11:38 +0100

    Create user.js
    update

---
+++ routes/user.js
@@ -0,0 +1,61 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+
+
+
+import { UtilisateurSignUp , login ,ProfilePicUpload ,DoctorSignUp, getAllUsers, ProfileEdit,   forgetPasssword, verifyOtp, resetPassword,sendOTP, DoctorInfos, getAllSpecialities } from '../controllers/user.js';
+ import { auth, authAdminSup ,authDoctor ,authUtilisateur  } from '../middlewares/auth.js'; 
+
+
+
+router
+  .route('/UtilisateurSignUp')
+  .post(UtilisateurSignUp);
+
+router
+  .route('/sendOTP')
+  .post(sendOTP)
+
+router
+  .route('/forgetPassword')
+  .post(forgetPasssword)
+
+  router
+  .route('/resetPassword')
+  .post(resetPassword)
+router
+  .route('/verifyOTP')
+  .post(verifyOtp)
+
+
+  router
+  .route('/DoctorSignup')
+  .post(DoctorSignUp);
+  router
+  .route('/DoctorInfos')
+  .patch(DoctorInfos);
+
+router
+  .route('/login')
+  .post(login);
+
+router
+  .route('/updatePicture')
+  .patch(auth,ProfilePicUpload);
+
+router
+  .route('/AllUsers')
+  .get(authAdminSup,getAllUsers)
+
+router
+  .route('/editProfile')
+  .patch(auth,ProfileEdit)
+
+  
+  
+
+
+ export default  router;
